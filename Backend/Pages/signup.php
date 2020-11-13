f0c8be0a2bf18c3fd8778b05d35f7a87dc3806c4
Bruno Taletti <31966406+brunotaletti24@users.noreply.github.com>
2020-11-13 14:30:34 -0300

    Changes v10

--- Backend/Pages/signup.php
+++ Backend/Pages/signup.php
@@ -3,23 +3,24 @@
 include_once("../Database/connection.php");
 
 // Puxa os itens do forulario e trata para evitar ataque sqli
-$firstname = mysqli_real_escape_string($conn, $_POST['first_name']);
-$lastname = mysqli_real_escape_string($conn, $_POST['last_name']);
-$username = mysqli_real_escape_string($conn, $_POST['username']);
-$email = mysqli_real_escape_string($conn, $_POST['email']);
-$phone = mysqli_real_escape_string($conn, $_POST['phone']);
-$password = mysqli_real_escape_string($conn, $_POST['password']);
+$firstname = $_POST['first_name'];
+$lastname = $_POST['last_name'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$password = $_POST['password'];
 $password = md5($password);
 
 // Executa a query para adicionar o usuario ao banco
-$query = "INSERT INTO users (first_name, last_name, username, email, phone, password, access_level) VALUES ('$firstname', '$lastname', '$username', '$email', '$phone', '$password', '1')";
-$sql = mysqli_query($conn, $query);
+$stmt = $db->prepare("INSERT INTO users (first_name, last_name, username, email, phone, password, access_level) VALUES ('$firstname', '$lastname', '$username', '$email', '$phone', '$password', '1')");
+$stmt->execute();
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
 
 // Verifica se o usuario ja existe e executa uma ação
-if(mysqli_insert_id($conn)){
-    header("Location: ../../Frontend/Pages/signin.php");
+if(!$result){
+    echo "usuario ja registrado!";
 } else{
-    echo "Usuário já existe!";
+    header("Location: ../../Frontend/Pages/signin.php");
 }
 
 ?>
--- README.txt
+++ README.txt
@@ -1,14 +1,16 @@
 [Frontend]
 - Html:
-
+	Comentar o código;
 - Css:
+	Comentar o código;
 
 - Js:
-
+	Comentar o código;
 [Backend]
-- PHP:  Mudar a conexão com o banco, utilizar PDO;
-	Pegar bloco de códigos repetitivos e joga-los em um arquivo separado, e chama-los em cada páginas atraves de um `include("arquivonome.php");`;
-	Alterar o nome das pastas para algo mais intuitivo;
-	Para buscas e coisas necessárias que seja exibido no URL usar METHOD GET;
+- PHP:
+	Mudar a conexão com o banco, utilizar PDO; OK 
+	Pegar bloco de códigos repetitivos e joga-los em um arquivo separado, e chama-los em cada páginas atraves de um `include("arquivonome.php")`; OK
+	Alterar o nome das pastas para algo mais intuitivo; OK
+	Para buscas e coisas necessárias que seja exibido no URL usar METHOD GET; OK
 
-- MySQL: Utilizar LIKE em campos de filtro;
+- MySQL:
