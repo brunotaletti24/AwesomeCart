6af87866bf6ef57030bc97e75078791035179657
Bruno Taletti <74149657+oBrunoTaletti@users.noreply.github.com>
2020-11-14 23:53:57 -0300

    chanves v11

--- Backend/Pages/settings.php
+++ Backend/Pages/settings.php
@@ -1,20 +1,42 @@
 <?php
-    //Incluindo a conexão com banco de dados   
-    include_once("../Database/connection.php"); 
 
-    // Inicia a sessão
-    session_start(); 
+// Inclui a conexão com banco de dados   
+include_once("../Database/connection.php"); 
 
-    // Passa os parametros capturados no formulário para as variaveis abaixo
-    $id = $_SESSION['id'];
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
+// Inicia a sessão
+session_start(); 
 
-    // Realiza a query com os dados informados pelo usuário
-    $stmt = $db->prepare("UPDATE users 
+// Passa os parametros capturados no formulário para as variaveis abaixo
+$id = $_SESSION['id'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+    
+// Verifica se os dados estão disponiveis
+
+// Procura o Usuário
+$verify_username = $db->prepare("SELECT username FROM users WHERE username='$username'");
+$verify_username->execute();
+$verify_username_result = $verify_username->fetchColumn();
+
+// Procura o Email
+$verify_email = $db->prepare("SELECT email FROM users WHERE email='$email'");
+$verify_email->execute();
+$verify_email_result = $verify_email->fetchColumn();
+
+// Faz a verificação
+if($verify_username_result > 0 && $verify_email_result > 0){ // Verifica email e usuarios iguais
+    echo "Sorry, this username and email is already registred!";
+} else if($verify_username_result > 0){ // Verifica usuário igual
+    echo "Sorry, username is already registred!";
+} else if($verify_email_result > 0){ // Verifica email iguail
+    echo "Sorry, this email already registred!";
+} else{  // Caso esteja tudo certo
+
+    // Atualiza os dados do usuário no banco
+    $query = $db->prepare("UPDATE users 
     SET first_name='$firstname',
     last_name='$lastname',
     first_name='$firstname',
@@ -23,10 +45,18 @@
     phone='$phone'
     WHERE id='$id'
     LIMIT 1");
+    $query->execute();
+    $result = $query->fetch(PDO::FETCH_ASSOC);
 
-    // Atualiza a tabela users com os parametros informados pelo usuario
-    $stmt->execute();
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    // Atualiza os dados da sessão
+    $_SESSION['firstname'] = $firstname;
+    $_SESSION['lastname'] = $lastname;
+    $_SESSION['username'] = $username;
+    $_SESSION['email'] = $email;
+    $_SESSION['phone'] = $phone;
 
-    header("javascript:history.back()");
+    // Leva o usuário de volta a página de ajustes
+    header("Location:javascript://history.go(-1)");
+}
+
 ?>
