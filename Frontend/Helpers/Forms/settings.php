101321afba6e6c9b41da1fa649abd2589916f609
Bruno Taletti <74149657+oBrunoTaletti@users.noreply.github.com>
2020-11-15 13:39:07 -0300

    changes v12

--- Frontend/Helpers/Forms/settings.php
+++ Frontend/Helpers/Forms/settings.php
@@ -1,9 +1,19 @@
 <form method="POST" action="../../Backend/Pages/settings.php">
     <input type="text" placeholder="First name" name="firstname" value="<?php echo $_SESSION["firstname"]; ?>">
+    
     <input type="text" placeholder="Last name" name="lastname" value="<?php echo $_SESSION["lastname"]; ?>">
+    
     <input type="text" placeholder="Username" name="username" value="<?php echo $_SESSION["username"]; ?>">
+    
     <input type="text" placeholder="Email" name="email" value="<?php echo $_SESSION["email"]; ?>">
+    
     <input type="text" placeholder="Phone number" name="phone" id="phone" value="<?php echo $_SESSION["phone"]; ?>">
+    
     <button class="btn btn-purple btn-block" type="submit">Change</button>
+         
+    <a href="#">
+        <button class="btn btn-red">Delete my account</button>
+    </a>
+
     <script src="../Js/phone.js"></script>
 </form>
